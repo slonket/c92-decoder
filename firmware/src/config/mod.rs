b61534d62e0d6ac0479094f7ae233e3bb6d9ed9f
wolfenick <ava.codie.wolfe@gmail.com>
2026-09-12 17:29:51 +1200

    added coreless motor config

--- firmware/src/config/mod.rs
+++ firmware/src/config/mod.rs
@@ -1,4 +1,9 @@
+#[cfg(feature = "marklin")]
 mod marklin;
+#[cfg(feature = "coreless")]
+mod coreless;
 
 #[cfg(feature = "marklin")]
 pub use marklin::*;
+#[cfg(feature = "coreless")]
+pub use coreless::*;
